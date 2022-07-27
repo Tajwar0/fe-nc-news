d863743a15a0a966db74e71b09d518d9162690b1
Tajwar0 <tajwar_h@hotmail.com>
2022-07-27 16:11:01 +0100

    implement working vote buttons

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,35 +1,31 @@
 import { useState, useEffect } from "react";
-import Articles from "./articles/Articles";
 const axios = require("axios").default;
 
 export default function Votes({ articleVotes, article_id }) {
   const [localVotes, setLocalVotes] = useState(0);
-  console.log(articleVotes, "votes");
-  function patchVotes() {
-    return axios
-      .patch(`https://tajwars-news.herokuapp.com/api/articles/${article_id}`, {
-        inc_votes: -1,
-      })
-      .then((data) => {
-        console.log(data);
-        return data;
-      });
-  }
+
+  useEffect(() => {
+    axios.patch(
+      `https://tajwars-news.herokuapp.com/api/articles/${article_id}`,
+      {
+        inc_votes: localVotes,
+      }
+    );
+  }, [localVotes]);
 
   function handleClickUp(event) {
     event.preventDefault();
     setLocalVotes(1);
-    patchVotes();
   }
   function handleClickDown(event) {
     event.preventDefault();
     setLocalVotes(-1);
-    console.log(localVotes);
-    patchVotes();
   }
   return (
     <div>
-      <b>Votes: {articleVotes + localVotes} </b>
+      <p className="votes">
+        <b>Votes: {articleVotes + localVotes} </b>
+      </p>
       <button disabled={localVotes !== 0} onClick={handleClickUp}>
         Vote Up
       </button>

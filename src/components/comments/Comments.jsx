e788cc98b91600a2c760f8f6312b5dc4a364bdec
Tajwar0 <tajwar_h@hotmail.com>
2022-07-28 16:53:11 +0100

    added skeleteon component for new comment

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import NewComment from "./NewComment";
 const axios = require("axios").default;
 
 export default function Comments({ article_id }) {
@@ -12,12 +13,18 @@ export default function Comments({ article_id }) {
         setArticleComments(response.data.articleComments);
       });
   }, [article_id]);
+
+  function handleButtonClick() {
+    // take user to bottom of page
+  }
   if (articleComments === undefined) {
     return [];
   } else {
     return (
       <div className="commentsSection">
+        {/* <button>{newCommentForm()}</button> */}
         <h3>Comments section</h3>
+        <NewComment article_id={article_id} />
         <ul className="commentsList">
           {articleComments.map((comment) => {
             return (
@@ -42,6 +49,7 @@ export default function Comments({ article_id }) {
             );
           })}
         </ul>
+        <NewComment />
       </div>
     );
   }
---
+++ src/components/comments/NewComment.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+
+export default function NewComment({ article_id }) {
+  return <div>add new comment</div>;
+}
